04f370a06a9b0456bc366948585005ee24bdc475
Maik Schmaddebeck <maik.schmaddebeck@icloud.com>
2020-01-25 10:16:01 +0100

    added icon component

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,24 +1,27 @@
 <script>
-  import { faPhone } from "@fortawesome/free-solid-svg-icons/faPhone";
-  import { faAt } from "@fortawesome/free-solid-svg-icons/faAt";
-  import { faWrench } from "@fortawesome/free-solid-svg-icons/faWrench";
-  import { faClock } from "@fortawesome/free-solid-svg-icons/faClock";
+  import { faAt, faFax, faPhone, faMobile, faWrench, faClock, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
   export let icon;
   export let size = 1;
 
   let path;
-  const height = `${size}em`;
+  let viewBoxX;
+  let viewBoxY;
 
   const dict = {
     at: faAt,
     clock: faClock,
+    fax: faFax,
+    filePdf: faFilePdf,
+    mobile: faMobile,
     phone: faPhone,
-    wrench: faWrench
+    wrench: faWrench,
   };
 
   const faIcon = dict[icon];
   if (faIcon) {
+    viewBoxX = faIcon.icon[0];
+    viewBoxY = faIcon.icon[1];
     path = faIcon.icon[4];
   } else {
     console.error(`Trying to use not imported icon ${icon}`);
@@ -28,11 +31,16 @@
 <style>
   svg {
     fill: currentColor;
+    width: 1em;
+    display: inline-block;
+    font-size: inherit;
+    overflow: visible;
+    vertical-align: -0.125em;
   }
 </style>
 
 {#if path}
-  <svg viewBox="0 0 576 512" {height}>
+  <svg viewBox="0 0 {viewBoxX} {viewBoxY}" style="height: {size}em">
     <path d={path} />
   </svg>
 {/if}
