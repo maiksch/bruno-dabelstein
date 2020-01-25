72ebab36fdc918f77eebd870d1484903fd6f586b
Maik Schmaddebeck <maik.schmaddebeck@icloud.com>
2020-01-25 10:16:20 +0100

    added anfahrt page

---
+++ src/routes/anfahrt.svelte
@@ -0,0 +1,18 @@
+<section class="text-center">
+  <div class="d-flex flex-column justify-content-center">
+    <h1>Hier finden Sie uns:</h1>
+    <address class="h-card">
+      <span class="p-name d-block">Bruno Dabelstein Stahl- und Maschinenbau GmbH</span>
+      <span class="p-street-address d-block">Otto-Hahn-Straße 2</span>
+      <span class="p-postal-code">D-21509</span>
+      <span class="p-locality">Glinde</span>
+    </address>
+  </div>
+</section>
+
+<iframe
+  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2371.5858214204018!2d10.2344937!3d53.5294535!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b1f3a782951157%3A0xfb924477b6d0b15e!2sOtto-Hahn-Stra%C3%9Fe+2%2C+21509+Glinde!5e0!3m2!1sde!2sde!4v1423587740318"
+  width="100%"
+  height="50%"
+  title="Google Maps Anfahrt"
+  frameborder="0" />
